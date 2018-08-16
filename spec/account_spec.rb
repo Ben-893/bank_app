876642128b5e5879447b415c724f9e8138262dfc
Benjamin <ben.smith.gordon@gmail.com>
2018-08-16 11:17:05 +0100

    Added some lines to the transactions test to test that a new transaction can be stored

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -11,12 +11,14 @@ end
 describe 'withdraw' do
   it 'deducts the amount from the balance' do
     expect { subject.withdraw(50) }.to change { subject.balance }.by(-50)
-    end
   end
+end
 
 describe 'transactions' do
   it 'is an empty array' do
     expect(subject.transactions).to be_empty
+    subject.deposit(50)
+    expect(subject.transactions).to_not be_empty
     end
   end
 end
